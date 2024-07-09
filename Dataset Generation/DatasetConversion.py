07c00ce976cfcfea021ee9c0bad69ac2c42b2e0f
Ben <ben.walters@my.jcu.edu.au>
2024-07-09 12:05:42 +1000

    Edited Dataset Conversion

--- Dataset Generation/DatasetConversion.py
+++ Dataset Generation/DatasetConversion.py
@@ -13,15 +13,9 @@ TestDict = pickle.load(f)
 f.close()
 
 
-file = h5py.File('NeuroMorse_Train.h5','w')
-file_test = h5py.File('NeuroMorse_Test.h5','w')
 
-dt = h5py.vlen_dtype(np.dtype('int32'))
-dt_labels = h5py.string_dtype()
-Label_group = file_test.create_group('Labels' )
-LabelsDataset = Label_group.create_dataset('Labels',(50,),dtype = dt_labels)
-StartTimesDataset = Label_group.create_dataset('Start Times',(50,),dtype = dt)
-EndTimesDataset = Label_group.create_dataset('End Times',(50,),dtype = dt)
+
+
 
 
 for d in Dropout:
@@ -38,6 +32,9 @@ for d in Dropout:
             Dataset = pickle.load(f)
             f.close()
 
+            file = h5py.File('Train/%s.h5' %(name),'w')
+            file_test = h5py.File('Test/%s.h5' %(name),'w')
+
             Spikes = []
             Channels = []
             Labels = []
@@ -53,14 +50,14 @@ for d in Dropout:
                 Labels.append(idx[1])
 
             
-            Spike_group = file.create_group('%s/Spikes' %(name))
+            Spike_group = file.create_group('Spikes')
             dt = h5py.vlen_dtype(np.dtype('int32'))
             dt_labels = h5py.string_dtype()
 
             SpikeDataset = Spike_group.create_dataset('Times',(50,),dtype = dt) 
             ChannelDataset = Spike_group.create_dataset('Channels',(50,),dtype = dt)
 
-            Label_group = file.create_group('%s/Labels'%(name))
+            Label_group = file.create_group('Labels')
             LabelDataset = Label_group.create_dataset('Labels',(50,),dtype = dt_labels)
 
             for i in range(50):
@@ -74,7 +71,7 @@ for d in Dropout:
             TestDataset = pickle.load(f)
             f.close()
 
-            Spike_group = file_test.create_group('%s/Spikes' %(name))
+            Spike_group = file_test.create_group('Spikes' %(name))
             
             dt = h5py.vlen_dtype(np.dtype('int32'))
             dt_labels = h5py.string_dtype()
@@ -82,20 +79,27 @@ for d in Dropout:
             TimeDataset = Spike_group.create_dataset('Times',(1,),dtype = dt)
             ChannelDataset = Spike_group.create_dataset('Channels',(1,), dtype = dt)
 
+            dt = h5py.vlen_dtype(np.dtype('int32'))
+            dt_labels = h5py.string_dtype()
+            Label_group = file_test.create_group('Labels' )
+            LabelsDataset = Label_group.create_dataset('Labels',(50,),dtype = dt_labels)
+            StartTimesDataset = Label_group.create_dataset('Start Times',(50,),dtype = dt)
+            EndTimesDataset = Label_group.create_dataset('End Times',(50,),dtype = dt)
+
             TimeList = []
             ChannelList = []
 
-            for event in TestDataset[0][0]:
+            for event in TestDataset[0]:
                 TimeList.append(event[0])
                 ChannelList.append(event[1])
             
             TimeDataset[0] = np.array(TimeList)
             ChannelDataset[0] = np.array(ChannelList)
 
-for idx,key in enumerate(TestDataset[0][1]):
-    LabelsDataset[idx] = key
-    StartTimesDataset[idx] = np.array(TestDict[key][1])
-    EndTimesDataset[idx] = np.array(TestDict[key][2])
+            for idx,key in enumerate(TestDataset[1]):
+                LabelsDataset[idx] = key
+                StartTimesDataset[idx] = np.array(TestDict[key][1])
+                EndTimesDataset[idx] = np.array(TestDict[key][2])
 
 
 
